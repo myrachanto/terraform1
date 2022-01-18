49e6503d64c3232860200d0946d4e80b44663d6b
myrachanto <myrachanto@gmail.com>
2022-01-18 14:21:24 +0300

    more changes

--- main.tf
+++ main.tf
@@ -21,3 +21,20 @@ resource "aws_subnet" "myapp-subnet-1" {
     "Name" = "${var.env_prefix}-subnet-1"
   }
 }
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+}
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    "Name" = "${var.env_prefix}-igateway"
+  }
+}
+resource "aws_route_table_association" "a-rtb_subnet" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
