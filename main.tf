ddf5bfc43797ef3e8b4310294dc7dbe36c971a55
myrachanto <myrachanto@gmail.com>
2022-01-19 12:06:57 +0300

    addd ec2 instance

--- main.tf
+++ main.tf
@@ -6,6 +6,9 @@ variable vpc_cidr_blocks {}
 variable subnet_cidr_blocks {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable ssh_key {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_blocks
@@ -38,3 +41,94 @@ resource "aws_route_table_association" "a-rtb_subnet" {
   subnet_id = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
 }
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+  tags = {
+    "Name" = "${var.env_prefix}-sg"
+  }
+}
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-arm64-gp2"]
+  }
+  # filter {
+  #   name = "virtualization-type"
+  #   values = ["hvm"]
+  # }
+}
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+output "ec2_public_ip" {
+  value = aws_instance.myapp_server.public_ip
+}
+resource "aws_instance" "myapp_server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [ aws_security_group.myapp-sg.id ]
+  availability_zone = var.avail_zone
+
+
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+
+  tags = {
+    "Name" = "${var.env_prefix}-server"
+  }
+}
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = file(var.ssh_key)
+}
+
+# //using default security group
+# resource "aws_default_security_group" "myapp-sg" {
+#   vpc_id = aws_vpc.myapp-vpc.id
+#   ingress {
+#     from_port = 22
+#     to_port = 22
+#     protocol = "tcp"
+#     cidr_blocks = [var.my_ip]
+#   }
+#   ingress {
+#     from_port = 8080
+#     to_port = 8080
+#     protocol = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     from_port = 0
+#     to_port = 0
+#     protocol = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     prefix_list_ids = []
+#   }
+#   tags = {
+#     "Name" = "${var.env_prefix}-default-sg"
+#   }
+# }
